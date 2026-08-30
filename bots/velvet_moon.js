393a664b8db44dc27de696783792c9bb7206e295
Airo CaeMDe <annabellepork@proton.me>
2026-08-31 02:40:26 +0300

    save

---
+++ bots/velvet_moon.js
@@ -0,0 +1,23 @@
+/**
+ * Відповіді бота velvet_moon
+ * Редагуй тільки firstMessage / autoReply / keywordReplies / singleWordReplies
+ */
+(function () {
+  "use strict";
+  window.HoomenBotReplies = window.HoomenBotReplies || {};
+  window.HoomenBotReplies.velvet_moon = {
+    firstMessage: "Схоже, velvet_moon давно не було в мережі. Вона отримає ваше повідомлення коли з'явиться в мережі.",
+    autoReply: [
+      "Схоже, velvet_moon давно не було в мережі. Вона отримає ваше повідомлення коли з'явиться в мережі."
+    ],
+    keywordReplies: {
+      "питання": [
+        "Схоже, velvet_moon давно не було в мережі. Вона отримає ваше повідомлення коли з'явиться в мережі."
+
+      ]
+    },
+    singleWordReplies: {
+      "Схоже, velvet_moon давно не було в мережі. Вона отримає ваше повідомлення коли з'явиться в мережі."
+    }
+  };
+})();
