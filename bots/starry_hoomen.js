5b73eb3a1ce093d873e779456bf2e59d14cceb18
Airo CaeMDe <annabellepork@proton.me>
2026-08-31 18:08:09 +0300

    s

---
+++ bots/starry_hoomen.js
@@ -0,0 +1,23 @@
+/**
+ * Відповіді бота starry_hoomen
+ * Редагуй тільки firstMessage / autoReply / keywordReplies / singleWordReplies
+ */
+(function () {
+  "use strict";
+  window.HoomenBotReplies = window.HoomenBotReplies || {};
+  window.HoomenBotReplies.starry_hoomen = {
+    firstMessage: "Схоже, starry_hoomen давно не було в мережі. Вона отримає ваше повідомлення коли з'явиться в мережі.",
+    autoReply: [
+      "Схоже, starry_hoomen давно не було в мережі. Вона отримає ваше повідомлення коли з'явиться в мережі."
+    ],
+    keywordReplies: {
+      "питання": [
+        "Схоже, starry_hoomen давно не було в мережі. Вона отримає ваше повідомлення коли з'явиться в мережі."
+
+      ]
+    },
+    singleWordReplies: {
+      "Привіт" : "Схоже, starry_hoomen давно не було в мережі. Вона отримає ваше повідомлення коли з'явиться в мережі."
+    }
+  };
+})();
